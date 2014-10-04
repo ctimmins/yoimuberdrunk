883f36bc7e580d305efeda70c643cf0fcf82d195
Jack Ketcham <kcajketcham@gmail.com>
2014-10-04 02:37:09 -0700

    No more firebase, just mongo + passport

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('drunkrawlApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
